1e48de95e6e47650639bb64a87a6e40df807458e
thoph <thoph@dev.ndex.net>
2023-12-03 01:25:40 +0700

    model

---
+++ Models/ClientDomainModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebNails.Payment.Models
+{
+    public class ClientDomainModel
+    {
+        public long ID { get; set; }
+        public string ClientDomain { get; set; }
+        public string Email { get; set; }
+        public int MiniumAmount { get; set; } 
+    }
+}
---
+++ Models/Payment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebNails.Payment.Models
+{
+    public class Payment
+    {
+        public long ID { get; set; }
+        public TypePayment TypePayment { get; set; }
+        public string Code { get; set; }
+        public string Transaction { get; set; }
+        public string EmailOwner { get; set; }
+        public string EmailBuyer { get; set; }
+        public string EmailReceiver { get; set; }
+        public int Amount { get; set; }
+        public bool IsRefund { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+
+    public enum TypePayment
+    {
+        Paypal = 1
+    }
+}
