7a52f571d74bb1925b1d0edcb3145e5d790644aa
thoph <thoph@dev.ndex.net>
2023-12-03 00:49:38 +0700

    router

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Net;
+using System.Web;
 using System.Web.Mvc;
 
 namespace WebNails.Payment
@@ -8,6 +9,7 @@ namespace WebNails.Payment
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
     }
 }
--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,9 +14,9 @@ namespace WebNails.Payment
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Main Site",
+                url: "{SiteName}/{Controller}/{Action}/{Id}",
+                defaults: new { SiteName = "system.nail", Controller = "home", Action = "index", Id = UrlParameter.Optional }
             );
         }
     }
